e3a61eb1454149034412a4577867621818e8f351
happysonsad <[kinchun917@gmail.com]>
2024-08-20 17:05:20 +0800

    draft the pseudocode of the project

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello World!")
+
+//There are two players in the game: (1)human and (2)computer
+
+//Write the logic to get the computer choice
+
+//Write the logic to get the human choice
+
+//Declare the players score variables
+
+//Write the logic to play a single round
+
+//Write the logic to play the entire game
+
