44c22dbdf6770a2432e82130291c8d12afb60137
happysonsad <[kinchun917@gmail.com]>
2024-08-20 21:54:33 +0800

    create a function to get the human's choice: -use prompt method to get the human's input and use .toLowerCase to prevent case sensitive issue

--- javascript.js
+++ javascript.js
@@ -16,7 +16,22 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 //Write the logic to get the human choice
-//
+//use the "prompt" to get user's input
+function getHumanChoice() {
+    let HumanChoice = prompt("Choose now! Rock, paper or scissors?","Rock");
+    if(HumanChoice.toLowerCase() === "rock") {
+        return "rock";
+    } else if (HumanChoice.toLowerCase() === "paper") {
+        return "paper";
+    } else if (HumanChoice.toLowerCase() === "scissors") {
+        return "scissors";
+    } else {
+        return alert("You should enter paper, rock or scissors! Refresh and try again!")
+    }
+
+}
+
+console.log(getHumanChoice());
 
 //Declare the players score variables
 
